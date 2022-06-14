f21a44981237dd2663fbd12305a8f7871f05cebd
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-13 23:15:54 -0700

    navbar fully functioning

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Navbar from "./components/navigation/Navbar";
 import Intro from "./components/intro/Intro";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss";
+import { useState } from "react";
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Navbar/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Works/>
