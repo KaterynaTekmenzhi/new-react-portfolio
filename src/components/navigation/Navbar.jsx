3094af502e8761f8810db85e9324a845f2c5636c
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-13 21:34:17 -0700

    left part of navbar complete

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import "./navbar.scss"
+import { MdAccountCircle, MdEmail } from 'react-icons/md';
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Genius.</a>
+          <div className="itemContainer">
+            <MdAccountCircle className="icon"/>
+            <span>+253-682-9029</span>
+          </div>
+          <div className="itemContainer">
+            <MdEmail className="icon"/>
+            <span>kateryna.tekmenzhi@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+        </div>
+      </div>
     </div>
   )
 }
+
+
