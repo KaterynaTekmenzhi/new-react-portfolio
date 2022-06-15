422d4aa81ac08f8f2a58200653ba5bf1917a9812
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-14 21:44:42 -0700

    app works

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import "./portfolio.scss"
+import React from 'react';
+import './portfolio.scss';
 
 export default function Portfolio({ projectData }) {
-    console.log(projectData, 'projectData');
-    return (
-      <div className="portfolio" id="portfolio">
-        <h1>Portfolio</h1>
-        <div className="container">
-          {projectData.map((project, index) => (
-            <div className="item" key={index}>
-              <img src={project.image} alt="something here" />
-              <h3>{project.title}</h3>
-            </div>
-          ))}
-        </div>
+  console.log(projectData, 'projectData');
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <div className="container">
+        {projectData.map((project, index) => (
+          <div className="item" key={index}>
+            <img src={require(`../../assets/${project.image}`)} alt="something here" />
+            <h3>{project.title}</h3>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
